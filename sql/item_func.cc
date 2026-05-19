MariaDB/server

commit ae33ebe5b32a82629a40e51c8d6c6611842fbd03

MDEV-23525 Wrong result of MIN(time_expr) and MAX(time_expr) with GROUP BY
Problem:

When calculatung MIN() and MAX() in a query with GROUP BY, like this:

  SELECT MIN(time_expr), MAX(time_expr) FROM t1 GROUP BY i;

the code in Item_sum_min_max::update_field() erroneosly used
string format comparison, therefore '100:20:30' was considered as
smaller than '10:20:30'.

Fix:

1. Implementing low level "native" related methods in class Time:
     Time::Time(const Native &native)           - convert native to Time
     Time::to_native(Native *to, uint decimals) - convert Time to native

   The "native" binary representation for TIME is equal to
   the binary data format of Field_timef, which is used to
   store TIME when mysql56_temporal_format is ON (default).

2. Implementing Type_handler_time_common "native" related methods:

  Type_handler_time_common::cmp_native()
  Type_handler_time_common::Item_val_native_with_conversion()
  Type_handler_time_common::Item_val_native_with_conversion_result()
  Type_handler_time_common::Item_param_val_native()

3. Implementing missing "native representation" related methods
   in Field_time and Field_timef:

  Field_time::store_native()
  Field_time::val_native()
  Field_timef::store_native()
  Field_timef::val_native()

4. Implementing missing "native" related methods in all Items
   that can have the TIME data type:

  Item_timefunc::val_native()
  Item_name_const::val_native()
  Item_time_literal::val_native()
  Item_cache_time::val_native()
  Item_handled_func::val_native()

5. Marking Type_handler_time_common as "native ready".
   So now Item_sum_min_max::update_field() calculates
   values using min_max_update_native_field(),
   which uses native binary representation rather than string representation.

   Before this change, only the TIMESTAMP data type used native
   representation to calculate MIN() and MAX().

Benchmarks (see more details in MDEV):

  This change not only fixes the wrong result, but also

sql/item_timefunc.h
@@ -598,6 +598,10 @@ public:
   double val_real() { return Time(this).to_double(); }
   String *val_str(String *to) { return Time(this).to_string(to, decimals); }
   my_decimal *val_decimal(my_decimal *to) { return Time(this).to_decimal(to); }
+  bool val_native(THD *thd, Native *to)
+  {
+    return Time(thd, this).to_native(to, decimals);
+  }
 };
 
 

sql/sql_type.cc
@@ -628,6 +628,23 @@ uint Interval_DDhhmmssff::fsp(THD *thd, Item *item)
 }
 
 
+bool Time::to_native(Native *to, uint decimals) const
+{
+  if (!is_valid_time())
+  {
+    to->length(0);
+    return true;
+  }
+  uint len= my_time_binary_length(decimals);
+  if (to->reserve(len))
+    return true;
+  longlong tmp= TIME_to_longlong_time_packed(get_mysql_time());
+  my_time_packed_to_binary(tmp, (uchar*) to->ptr(), decimals);
+  to->length(len);
+  return false;
+}
+
+
 void Time::make_from_item(THD *thd, int *warn, Item *item, const Options opt)
 {
   *warn= 0;
@@ -809,6 +826,28 @@ void Time::make_from_time(int *warn, const MYSQL_TIME *from)
     *(static_cast<MYSQL_TIME*>(this))= *from;
     adjust_time_range_or_invalidate(warn);
   }
+}
+
+
+uint Time::binary_length_to_precision(uint length)
+{
+  switch (length) {
+  case 3: return 0;
+  case 4: return 2;
+  case 5: return 4;
+  case 6: return 6;
+  }
+  DBUG_ASSERT(0);
+  return 0;
+}
+
+
+Time::Time(const Native &native)
+{
+  uint dec= binary_length_to_precision(native.length());
+  longlong tmp= my_time_packed_from_binary((const uchar *) native.ptr(), dec);
+  TIME_from_longlong_time_packed(this, tmp);
+  DBUG_ASSERT(is_valid_time());
 }
 
 
@@ -1453,6 +1492,13 @@ const Type_handler *
 Type_handler_timestamp_common::type_handler_for_native_format() const
 {
   return &type_handler_timestamp2;
+}
+
+
+const Type_handler *
+Type_handler_time_common::type_handler_for_native_format() const
+{
+  return &type_handler_time2;
 }
 
 
@@ -8391,6 +8437,51 @@ Type_handler_time_common::create_literal_item(THD *thd,
 }
 
 
+bool
+Type_handler_time_common::Item_val_native_with_conversion(THD *thd,
+                                                          Item *item,
+                                                          Native *to) const
+{
+  if (item->type_handler()->type_handler_for_native_format() ==
+      &type_handler_time2)
+    return item->val_native(thd, to);
+  return Time(thd, item).to_native(to, item->time_precision(thd));
+}
+
+
+bool
+Type_handler_time_common::Item_val_native_with_conversion_result(THD *thd,
+                                                                 Item *item,
+                                                                 Native *to)
+                                                                 const
+{
+  if (item->type_handler()->type_handler_for_native_format() ==
+      &type_handler_time2)
+    return item->val_native_result(thd, to);
+  MYSQL_TIME ltime;
+  if (item->get_date_result(thd, &ltime, Time::Options(thd)))
+    return true;
+  int warn;
+  return Time(&warn, &ltime, 0).to_native(to, item->time_precision(thd));
+}
+
+
+int Type_handler_time_common::cmp_native(const Native &a,
+                                         const Native &b) const
+{
+  // Optimize a simple case: equal fractional precision:
+  if (a.length() == b.length())
+    return memcmp(a.ptr(), b.ptr(), a.length());
+  longlong lla= Time(a).to_packed();
+  longlong llb= Time(b).to_packed();
+  if (lla < llb)
+    return -1;
+  if (lla> llb)
+    return 1;
+  return 0;
+}
+
+
 bool Type_handler_timestamp_common::TIME_to_native(THD *thd,
                                                    const MYSQL_TIME *ltime,
                                                    Native *to,
@@ -8498,6 +8589,15 @@ Type_handler_timestamp_common::Item_param_val_native(THD *thd,
   return
     item->get_date(thd, &ltime, Datetime::Options(TIME_NO_ZERO_IN_DATE, thd)) ||
     TIME_to_native(thd, &ltime, to, item->datetime_precision(thd));
+}
+
+
+bool
+Type_handler_time_common::Item_param_val_native(THD *thd,
+                                                Item_param *item,
+                                                Native *to) const
+{
+  return Time(thd, item).to_native(to, item->decimals);
 }
 
 

mysql-test/main/type_time.result
@@ -2286,5 +2286,127 @@ SELECT '1972-11-06 16:58:58' < TIME'20:31:05';
 '1972-11-06 16:58:58' < TIME'20:31:05'
 1
 #
+# MDEV-23525 Wrong result of MIN(time_expr) and MAX(time_expr) with GROUP BY
+#
+SET timestamp=UNIX_TIMESTAMP('2020-08-21 18:19:20');
+CREATE PROCEDURE p1()
+BEGIN
+SELECT MIN(t), MAX(t) FROM t1;
+SELECT i, MIN(t), MAX(t) FROM t1 GROUP BY i;
+SELECT i, MIN(COALESCE(t)), MAX(COALESCE(t)) FROM t1 GROUP BY i;
+SELECT i, MIN(t+INTERVAL 1 SECOND), MAX(t+INTERVAL 1 SECOND) FROM t1 GROUP BY i;
+SELECT i, MIN(TIME'10:20:30'+INTERVAL 1 SECOND) FROM t1 GROUP BY i;
+SELECT i, MIN(CURRENT_TIME), MAX(CURRENT_TIME) FROM t1 GROUP BY i;
+SELECT
+i,
+MIN((SELECT MAX(CURRENT_TIME) FROM t1)),
+MAX((SELECT MAX(CURRENT_TIME) FROM t1))
+FROM t1 GROUP BY i;
+SELECT
+i,
+MIN(NAME_CONST('name',TIME'10:20:30')),
+MAX(NAME_CONST('name',TIME'10:20:30'))
+FROM t1 GROUP BY i;
+EXECUTE IMMEDIATE "SELECT i, MIN(?),MAX(?) FROM t1 GROUP BY i"
+                    USING TIME'10:20:30', TIME'10:20:30';
+END;
+$$
+CREATE TABLE t1 (i INT, t TIME);
+INSERT INTO t1 VALUES (1,'10:20:30');
+INSERT INTO t1 VALUES (1,'100:20:20');
+CALL p1;
+MIN(t)	MAX(t)
+10:20:30	100:20:20
+i	MIN(t)	MAX(t)
+1	10:20:30	100:20:20
+i	MIN(COALESCE(t))	MAX(COALESCE(t))
+1	10:20:30	100:20:20
+i	MIN(t+INTERVAL 1 SECOND)	MAX(t+INTERVAL 1 SECOND)
+1	10:20:31	100:20:21
+i	MIN(TIME'10:20:30'+INTERVAL 1 SECOND)
+1	10:20:31
+i	MIN(CURRENT_TIME)	MAX(CURRENT_TIME)
+1	18:19:20	18:19:20
+i	MIN((SELECT MAX(CURRENT_TIME) FROM t1))	MAX((SELECT MAX(CURRENT_TIME) FROM t1))
+1	18:19:20	18:19:20
+i	MIN(NAME_CONST('name',TIME'10:20:30'))	MAX(NAME_CONST('name',TIME'10:20:30'))
+1	10:20:30	10:20:30
+i	MIN(?)	MAX(?)
+1	10:20:30	10:20:30
+DROP TABLE t1;
+CREATE TABLE t1 (i INT, t TIME(3));
+INSERT INTO t1 VALUES (1,'10:20:30.123');
+INSERT INTO t1 VALUES (1,'100:20:20.123');
+CALL p1;
+MIN(t)	MAX(t)
+10:20:30.123	100:20:20.123
+i	MIN(t)	MAX(t)
+1	10:20:30.123	100:20:20.123
+i	MIN(COALESCE(t))	MAX(COALESCE(t))
+1	10:20:30.123	100:20:20.123
+i	MIN(t+INTERVAL 1 SECOND)	MAX(t+INTERVAL 1 SECOND)
+1	10:20:31.123	100:20:21.123
+i	MIN(TIME'10:20:30'+INTERVAL 1 SECOND)
+1	10:20:31
+i	MIN(CURRENT_TIME)	MAX(CURRENT_TIME)
+1	18:19:20	18:19:20
+i	MIN((SELECT MAX(CURRENT_TIME) FROM t1))	MAX((SELECT MAX(CURRENT_TIME) FROM t1))
+1	18:19:20	18:19:20
+i	MIN(NAME_CONST('name',TIME'10:20:30'))	MAX(NAME_CONST('name',TIME'10:20:30'))
+1	10:20:30	10:20:30
+i	MIN(?)	MAX(?)
+1	10:20:30	10:20:30
+DROP TABLE t1;
+CREATE TABLE t1 (i INT, t TIME(6));
+INSERT INTO t1 VALUES (1,'10:20:30.123456');
+INSERT INTO t1 VALUES (1,'100:20:20.123456');
+CALL p1;
+MIN(t)	MAX(t)
+10:20:30.123456	100:20:20.123456
+i	MIN(t)	MAX(t)
+1	10:20:30.123456	100:20:20.123456
+i	MIN(COALESCE(t))	MAX(COALESCE(t))
+1	10:20:30.123456	100:20:20.123456
+i	MIN(t+INTERVAL 1 SECOND)	MAX(t+INTERVAL 1 SECOND)
+1	10:20:31.123456	100:20:21.123456
+i	MIN(TIME'10:20:30'+INTERVAL 1 SECOND)
+1	10:20:31
+i	MIN(CURRENT_TIME)	MAX(CURRENT_TIME)
+1	18:19:20	18:19:20
+i	MIN((SELECT MAX(CURRENT_TIME) FROM t1))	MAX((SELECT MAX(CURRENT_TIME) FROM t1))
+1	18:19:20	18:19:20
+i	MIN(NAME_CONST('name',TIME'10:20:30'))	MAX(NAME_CONST('name',TIME'10:20:30'))
+1	10:20:30	10:20:30
+i	MIN(?)	MAX(?)
+1	10:20:30	10:20:30
+DROP TABLE t1;
+SET @@global.mysql56_temporal_format=false;
+CREATE TABLE t1 (i INT, t TIME(6));
+INSERT INTO t1 VALUES (1,'10:20:30.123456');
+INSERT INTO t1 VALUES (1,'100:20:20.123456');
+CALL p1;
+MIN(t)	MAX(t)
+10:20:30.123456	100:20:20.123456
+i	MIN(t)	MAX(t)
+1	10:20:30.123456	100:20:20.123456
+i	MIN(COALESCE(t))	MAX(COALESCE(t))
+1	10:20:30.123456	100:20:20.123456
+i	MIN(t+INTERVAL 1 SECOND)	MAX(t+INTERVAL 1 SECOND)
+1	10:20:31.123456	100:20:21.123456
+i	MIN(TIME'10:20:30'+INTERVAL 1 SECOND)
+1	10:20:31
+i	MIN(CURRENT_TIME)	MAX(CURRENT_TIME)
+1	18:19:20	18:19:20
+i	MIN((SELECT MAX(CURRENT_TIME) FROM t1))	MAX((SELECT MAX(CURRENT_TIME) FROM t1))
+1	18:19:20	18:19:20
+i	MIN(NAME_CONST('name',TIME'10:20:30'))	MAX(NAME_CONST('name',TIME'10:20:30'))
+1	10:20:30	10:20:30
+i	MIN(?)	MAX(?)
+1	10:20:30	10:20:30
+DROP TABLE t1;
+SET @@global.mysql56_temporal_format=default;
+DROP PROCEDURE p1;
+SET timestamp=DEFAULT;
+#
 # End of 10.4 tests
 #

mysql-test/main/type_time.test
@@ -1491,5 +1491,65 @@ DROP TABLE t1;
 SELECT '1972-11-06 16:58:58' < TIME'20:31:05';
 
 --echo #
+--echo # MDEV-23525 Wrong result of MIN(time_expr) and MAX(time_expr) with GROUP BY
+--echo #
+
+SET timestamp=UNIX_TIMESTAMP('2020-08-21 18:19:20');
+
+DELIMITER $$;
+CREATE PROCEDURE p1()
+BEGIN
+  SELECT MIN(t), MAX(t) FROM t1;
+  SELECT i, MIN(t), MAX(t) FROM t1 GROUP BY i;
+  SELECT i, MIN(COALESCE(t)), MAX(COALESCE(t)) FROM t1 GROUP BY i;
+  SELECT i, MIN(t+INTERVAL 1 SECOND), MAX(t+INTERVAL 1 SECOND) FROM t1 GROUP BY i;
+  SELECT i, MIN(TIME'10:20:30'+INTERVAL 1 SECOND) FROM t1 GROUP BY i;
+  SELECT i, MIN(CURRENT_TIME), MAX(CURRENT_TIME) FROM t1 GROUP BY i;
+  SELECT
+    i,
+    MIN((SELECT MAX(CURRENT_TIME) FROM t1)),
+    MAX((SELECT MAX(CURRENT_TIME) FROM t1))
+  FROM t1 GROUP BY i;
+  SELECT
+    i,
+    MIN(NAME_CONST('name',TIME'10:20:30')),
+    MAX(NAME_CONST('name',TIME'10:20:30'))
+  FROM t1 GROUP BY i;
+  EXECUTE IMMEDIATE "SELECT i, MIN(?),MAX(?) FROM t1 GROUP BY i"
+                    USING TIME'10:20:30', TIME'10:20:30';
+END;
+$$
+DELIMITER ;$$
+
+CREATE TABLE t1 (i INT, t TIME);
+INSERT INTO t1 VALUES (1,'10:20:30');
+INSERT INTO t1 VALUES (1,'100:20:20');
+CALL p1;
+DROP TABLE t1;
+
+CREATE TABLE t1 (i INT, t TIME(3));
+INSERT INTO t1 VALUES (1,'10:20:30.123');
+INSERT INTO t1 VALUES (1,'100:20:20.123');
+CALL p1;
+DROP TABLE t1;
+
+CREATE TABLE t1 (i INT, t TIME(6));
+INSERT INTO t1 VALUES (1,'10:20:30.123456');
+INSERT INTO t1 VALUES (1,'100:20:20.123456');
+CALL p1;
+DROP TABLE t1;
+
+SET @@global.mysql56_temporal_format=false;
+CREATE TABLE t1 (i INT, t TIME(6));
+INSERT INTO t1 VALUES (1,'10:20:30.123456');
+INSERT INTO t1 VALUES (1,'100:20:20.123456');
+CALL p1;
+DROP TABLE t1;
+SET @@global.mysql56_temporal_format=default;
+
+DROP PROCEDURE p1;
+SET timestamp=DEFAULT;
+
+--echo #
 --echo # End of 10.4 tests
 --echo #

sql/field.cc
@@ -6011,6 +6011,24 @@ bool Field_time::get_date(MYSQL_TIME *ltime, date_mode_t fuzzydate)
 }
 
 
+int Field_time::store_native(const Native &value)
+{
+  Time t(value);
+  DBUG_ASSERT(t.is_valid_time());
+  store_TIME(t);
+  return 0;
+}
+
+
+bool Field_time::val_native(Native *to)
+{
+  MYSQL_TIME ltime;
+  get_date(&ltime, date_mode_t(0));
+  int warn;
+  return Time(&warn, &ltime, 0).to_native(to, decimals());
+}
+
+
 bool Field_time::send_binary(Protocol *protocol)
 {
   MYSQL_TIME ltime;
@@ -6253,6 +6271,22 @@ bool Field_timef::get_date(MYSQL_TIME *ltime, date_mode_t fuzzydate)
   TIME_from_longlong_time_packed(ltime, tmp);
   return false;
 }
+
+int Field_timef::store_native(const Native &value)
+{
+  DBUG_ASSERT(value.length() == my_time_binary_length(dec));
+  DBUG_ASSERT(Time(value).is_valid_time());
+  memcpy(ptr, value.ptr(), value.length());
+  return 0;
+}
+
+
+bool Field_timef::val_native(Native *to)
+{
+  uint32 binlen= my_time_binary_length(dec);
+  return to->copy((const char*) ptr, binlen);
+}
+
 
 /****************************************************************************
 ** year type

sql/field.h
@@ -3213,6 +3213,8 @@ public:
            decimals() == from->decimals();
   }
   sql_mode_t conversion_depends_on_sql_mode(THD *, Item *) const;
+  int store_native(const Native &value);
+  bool val_native(Native *to);
   int store_time_dec(const MYSQL_TIME *ltime, uint dec);
   int store(const char *to,size_t length,CHARSET_INFO *charset);
   int store(double nr);
@@ -3334,6 +3336,8 @@ public:
   }
   int reset();
   bool get_date(MYSQL_TIME *ltime, date_mode_t fuzzydate);
+  int store_native(const Native &value);
+  bool val_native(Native *to);
   uint size_of() const { return sizeof(*this); }
 };
 

sql/item.cc
@@ -2011,6 +2011,11 @@ bool Item_name_const::get_date(THD *thd, MYSQL_TIME *ltime, date_mode_t fuzzydat
   return rc;
 }
 
+bool Item_name_const::val_native(THD *thd, Native *to)
+{
+  return val_native_from_item(thd, value_item, to);
+}
+
 bool Item_name_const::is_null()
 {
   return value_item->is_null();

sql/item.h
@@ -1311,16 +1311,13 @@ public:
   {
    /*
      The default implementation for the Items that do not need native format:
-     - Item_basic_value
+     - Item_basic_value (default implementation)
      - Item_copy
      - Item_exists_subselect
      - Item_sum_field
      - Item_sum_or_func (default implementation)
      - Item_proc
      - Item_type_holder (as val_xxx() are never called for it);
-     - TODO: Item_name_const will need val_native() in the future,
-       when we add this syntax:
-         TIMESTAMP WITH LOCAL TIMEZONE'2001-01-01 00:00:00'
 
      These hybrid Item types override val_native():
      - Item_field
@@ -1331,6 +1328,8 @@ public:
      - Item_direct_ref
      - Item_direct_view_ref
      - Item_ref_null_helper
+     - Item_name_const
+     - Item_time_literal
      - Item_sum_or_func
          Note, these hybrid type Item_sum_or_func descendants
          override the default implementation:
@@ -3139,6 +3138,7 @@ public:
   String *val_str(String *sp);
   my_decimal *val_decimal(my_decimal *);
   bool get_date(THD *thd, MYSQL_TIME *ltime, date_mode_t fuzzydate);
+  bool val_native(THD *thd, Native *to);
   bool is_null();
   virtual void print(String *str, enum_query_type query_type);
 
@@ -4897,6 +4897,10 @@ public:
   String *val_str(String *to) { return Time(this).to_string(to, decimals); }
   my_decimal *val_decimal(my_decimal *to) { return Time(this).to_decimal(to); }
   bool get_date(THD *thd, MYSQL_TIME *res, date_mode_t fuzzydate);
+  bool val_native(THD *thd, Native *to)
+  {
+    return Time(thd, this).to_native(to, decimals);
+  }
   Item *get_copy(THD *thd)
   { return get_item_copy<Item_time_literal>(thd, this); }
 };
@@ -6871,6 +6875,10 @@ public:
   my_decimal *val_decimal(my_decimal *to)
   {
     return has_value() ? Time(this).to_decimal(to) : NULL;
+  }
+  bool val_native(THD *thd, Native *to)
+  {
+    return has_value() ? Time(thd, this).to_native(to, decimals) : true;
   }
 };
 

YuryYudin/MariaDB-server

commit 5056370403fb871727dd3574dfe1c5e7a16bde5e

MDEV-23676 Assertion in my_time_packed_to_binary via val_native
my_time_packed_to_binary() requires the fractional part of the packed
TIME to be already aligned to the requested `decimals` precision (the
Debug-build assertion at compat56.cc:126 enforces this). Six call
sites construct a Time from an Item via `Time(thd, item)` and feed
the result to `Time::to_native(to, decimals)`, but the no-dec Time
constructor does not apply the consumer-side precision: when the
Item's claimed decimals are smaller than the actual second_part
materialised by Item::get_date() (e.g. GREATEST/LEAST aggregating an
out-of-range integer-as-time literal against a TIME(N<6) field), the
asserter fires on a debug build.

Switch each of the six call sites to the existing dec-aware ctor
`Time(thd, item, Time::Options(thd), dec)` (and the analogous
`Time(int *warn, const MYSQL_TIME*, long curdays, Time::Options,
uint dec)` for the result / raw-MYSQL_TIME variant). That ctor's
terminal `round(dec, time_round_mode_t(opt))` dispatches to
`trunc(dec)` or `round(dec, warn)` based on
`THD::temporal_round_mode()`, so the user's
`MODE_TIME_ROUND_FRACTIONAL` sql_mode is honored end-to-end.

Affected callers:
  sql/item_timefunc.h   Item_timefunc::val_native
  sql/item_func.cc      Item_func_int_val::native_op
  sql/item_func.cc      Item_func_round::native_op
  sql/sql_type.cc       Type_handler_time_common::
                          Item_val_native_with_conversion
  sql/sql_type.cc       Type_handler_time_common::
                          Item_val_native_with_conversion_result
  sql/sql_type.cc       Type_handler_time_common::Item_param_val_native

The regression was introduced by ae33ebe5b32a (MDEV-23525, 10.4)
which added the val_native path on TIME-typed Items. A related
sibling assertion along the subselect-cache path was previously
addressed by 1a3859fff09 (MDEV-29924, 10.4.27+) via Item_cache::setup
metadata propagation; that fix did not cover the GROUP BY MIN/MAX

sql/item_func.cc
@@ -2363,7 +2363,7 @@ bool Item_func_int_val::native_op(THD *thd, Native *to)
 {
   // TODO: turn Item_func_int_val into Item_handled_func eventually.
   if (type_handler()->mysql_timestamp_type() == MYSQL_TIMESTAMP_TIME)
-    return Time(thd, this).to_native(to, decimals);
+    return Time(thd, this, Time::Options(thd), decimals).to_native(to, decimals);
   DBUG_ASSERT(0);
   return true;
 }
@@ -2820,7 +2820,7 @@ bool Item_func_round::native_op(THD *thd, Native *to)
 {
   // TODO: turn Item_func_round into Item_handled_func eventually.
   if (type_handler()->mysql_timestamp_type() == MYSQL_TIMESTAMP_TIME)
-    return Time(thd, this).to_native(to, decimals);
+    return Time(thd, this, Time::Options(thd), decimals).to_native(to, decimals);
   DBUG_ASSERT(0);
   return true;
 }

sql/item_timefunc.h
@@ -728,7 +728,7 @@ public:
   my_decimal *val_decimal(my_decimal *to) override
   { return Time(this).to_decimal(to); }
   bool val_native(THD *thd, Native *to) override
-  { return Time(thd, this).to_native(to, decimals); }
+  { return Time(thd, this, Time::Options(thd), decimals).to_native(to, decimals); }
 };
 
 

sql/sql_type.cc
@@ -9265,7 +9265,8 @@ Type_handler_time_common::Item_val_native_with_conversion(THD *thd,
   if (item->type_handler()->type_handler_for_native_format() ==
       &type_handler_time2)
     return item->val_native(thd, to);
-  return Time(thd, item).to_native(to, item->time_precision(thd));
+  uint dec= item->time_precision(thd);
+  return Time(thd, item, Time::Options(thd), dec).to_native(to, dec);
 }
 
 
@@ -9282,7 +9283,8 @@ Type_handler_time_common::Item_val_native_with_conversion_result(THD *thd,
   if (item->get_date_result(thd, &ltime, Time::Options(thd)))
     return true;
   int warn;
-  return Time(&warn, &ltime, 0).to_native(to, item->time_precision(thd));
+  uint dec= item->time_precision(thd);
+  return Time(&warn, &ltime, 0, Time::Options(thd), dec).to_native(to, dec);
 }
 
 
@@ -9424,7 +9426,8 @@ Type_handler_time_common::Item_param_val_native(THD *thd,
                                                 Item_param *item,
                                                 Native *to) const
 {
-  return Time(thd, item).to_native(to, item->decimals);
+  uint dec= item->decimals;
+  return Time(thd, item, Time::Options(thd), dec).to_native(to, dec);
 }
 
 

mysql-test/main/type_time_hires.result
@@ -916,6 +916,18 @@ select c from t1 where c<all (select d from t1);
 c
 drop table t1;
 #
+# MDEV-23676 Assertion `(((nr) % (1LL << 24)) % (int) log_10_int[6 - dec]) == 0' failed in my_time_packed_to_binary
+#
+create table t1 (a time(3));
+insert into t1 values (null),(null);
+select min(greatest(5237686366631886848, a)) from t1 group by a;
+min(greatest(5237686366631886848, a))
+NULL
+Warnings:
+Warning	1292	Incorrect time value: '5237686366631886848'
+Warning	1292	Incorrect time value: '5237686366631886848'
+drop table t1;
+#
 # End of 10.4 tests
 #
 #

mysql-test/main/type_time_hires.test
@@ -205,6 +205,14 @@ select c from t1 where c<all (select d from t1);
 drop table t1;
 
 --echo #
+--echo # MDEV-23676 Assertion `(((nr) % (1LL << 24)) % (int) log_10_int[6 - dec]) == 0' failed in my_time_packed_to_binary
+--echo #
+create table t1 (a time(3));
+insert into t1 values (null),(null);
+select min(greatest(5237686366631886848, a)) from t1 group by a;
+drop table t1;
+
+--echo #
 --echo # End of 10.4 tests
 --echo #
 